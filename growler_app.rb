b36fe17648d18c00bc3bf5faa41263dcb9eabdc7
James Rutledge <jarutledge2@gmail.com>
2015-05-22 17:37:59 -0500

    Created a csv file to store growls, added get growls functionality to the app.rb file, prints out in index.erb

--- growler_app.rb
+++ growler_app.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'csv'
+
+class Growl
+	attr_accessor :text
+	def initialize(text)
+		@text = text
+	end
+end
 
 get '/' do
+	arr_of_growls = []
+	CSV.foreach('growls.csv') do |row|
+		arr_of_growls << row[0]
+	end
+
+	# i = 0
+	# for index in arr_of_growls do
+	# 	p arr_of_growls[i]
+	# 	i += 1
+	# end
+
+	# growl_array = Growl.new('Testtesttest, another test, and yet another')
+	@growls = "#{arr_of_growls.join(', ')}"
 	erb :index
 end
